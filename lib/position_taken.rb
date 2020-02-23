af4b4b6f6d369acda1c7ee17476f26aef1b971fc
Jason Keeling <s2jason@yahoo.com>
2020-02-23 16:32:57 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if index[0] != " "  
-  elsif index[1] != " " 
-  elsif index[2] != " " 
-  elsif index[3] != " " 
-  elsif index[4] != " " 
-  elsif index[5] != " " 
-  elsif index[6] != " " 
-  elsif index[7] != " "
-  elsif index[8] != " " 
+  if index[index_number] != " "  
   end
 end
